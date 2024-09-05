fe1643676179314fb7edee07031c787a69bef974
andresRios <andres4715@gmail.com>
2024-09-05 06:41:41 -0500

    new addition 2 working fine

--- NewFolder/mytest8.js
+++ NewFolder/mytest8.js
@@ -3,3 +3,9 @@ console.log('checking rebase from github');
 console.log('This line was added after adding data on github');
 console.log('after revert');
 console.log('final version after revert');
+
+function data2() {
+  const data1 = 1;
+  const data2 = 2;
+  return data1 + data2;
+}
