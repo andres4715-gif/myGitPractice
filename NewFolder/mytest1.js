4b1ad5548676b846031612f41b2fcb40fc60b581
andresRios <andres4715@gmail.com>
2024-09-05 05:38:32 -0500

    this is the wrong commit

--- NewFolder/mytest1.js
+++ NewFolder/mytest1.js
@@ -1,1 +1,2 @@
 console.log('🚫 doing rebase test');
+console.log('✅ Rebase in progress');
--- NewFolder/mytest8.js
+++ NewFolder/mytest8.js
@@ -1,2 +1,3 @@
-console.log("version 8"); 
+console.log('version 8');
 console.log('checking rebase from github');
+console.log('This line was added after adding data on github');
