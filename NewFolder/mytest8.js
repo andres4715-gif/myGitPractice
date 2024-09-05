7c3373b3df5c220a25c38baff91b965f4d620f8c
andresRios <andres4715@gmail.com>
2024-09-05 06:21:53 -0500

    function to master

--- NewFolder/mytest8.js
+++ NewFolder/mytest8.js
@@ -3,3 +3,12 @@ console.log('checking rebase from github');
 console.log('This line was added after adding data on github');
 console.log('after revert');
 console.log('final version after revert');
+console.log('uncommitted changes✅');
+
+function thisIsJavaScriptFunction() {
+  console.log('is better to find something with javaScript');
+}
+
+function functionWithJava() {
+  console.log('this is a java function');
+}
