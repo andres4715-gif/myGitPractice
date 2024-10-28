a193e5e8751f8d347de2e226ec2fb70db3a5c3da
andresRios <andres4715@gmail.com>
2024-10-28 16:43:30 -0500

    new data

--- NewFolder/mytest9.js
+++ NewFolder/mytest9.js
@@ -1,5 +1,3 @@
 console.log('version 9');
 console.log('✈️ Rebase in progress_2 from VSCODE');
 console.log('⚙️ Data changed after some master changes');
-// change from github
-// change from vscode
